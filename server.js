26c25c140ebdf0c3f255a77e001386ecf6248ee1
OpenTech-Consult <opentech.consult@gmail.com>
2022-10-28 13:27:31 +0000

    Model View Controller in MVC

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express')
 
+const friendsController = require('./controllers/friends.controller')
+const messagesController = require('./controllers/messages.controller')
+
 const app = express()
 
 const PORT = 3000
-
-const friends = [
-    {
-        id: 0,
-        name: 'Albert Einstein'
-    },
-    {
-        id: 1,
-        name: 'Sir Isaac Newtoon'
-    }
-]
 
 app.use((req, res, next) => {
     const start = Date.now()
@@ -24,44 +16,13 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
-app.post('/friends', (req, res) => {
-    if (!req.body.name) {
-        return res.status(400).json({
-            error: 'Missing friend name'
-        })
-    }
-    const newFriend = {
-        id: friends.length,
-        name: req.body.name
-    }
-    friends.push(newFriend)
-    res.json(newFriend)
-})
-
-app.get('/friends', (req, res) => {
-    res.json(friends)
-})
-
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId)
-    const friend = friends[friendId]
-    if (friend) {
-        res.status(200).json(friend)
-    } else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        })
-    }
-})
+app.post('/friends', friendsController.postFriend)
+app.get('/friends', friendsController.getFriends)
+app.get('/friends/:friendId', friendsController.getFriend)
 
 
-app.get('/messages', (req, res) => {
-    res.send('<ul><li>Hello Albert!</li></ul>')
-})
-
-app.post('/messages', (req, res) => {
-    console.log('Updating messages...')
-})
+app.get('/messages', messagesController.getMessages)
+app.post('/messages', messagesController.postMessage)
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}...`)
